1b96b2024251557022c671bacef8a9dba2641176
Douglas <douglasekwule@gmail.com>
2022-11-30 07:41:41 -0800

    0x01-python-if_else_loops_functions task

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Douglas E. Ameh
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author - Douglas E. Ameh
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
